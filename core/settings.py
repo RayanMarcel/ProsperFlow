6812d011e41aec65a4945a1434fb4cf78d8296fa
rayanmpc <rayan@UbuntuServer.myguest.virtualbox.org>
2023-07-05 22:28:07 -0400

    criado o template base para construcao das funcionalidades

--- core/settings.py
+++ core/settings.py
@@ -86,3 +86,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
