b02af6fd9aa33f79625a1eba75887303fb933d1a
rayanmpc <rayan@UbuntuServer.myguest.virtualbox.org>
2023-07-06 18:54:54 -0400

    implementada models e choices de bancos e tipo

--- UserProfile/models.py
+++ UserProfile/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from UserProfile.choices import ChoicesBancos, ChoicesTipoConta
 
-# Create your models here.
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.categoria
+
+class Conta(models.Model):
+
+    apelido = models.CharField(max_length=50)
+    banco = models.CharField(max_length=3, choices=ChoicesBancos.bancos)
+    tipo = models.CharField(max_length=2, choices=ChoicesTipoConta.tipo)
+    valor = models.FloatField()
+    icone = models.ImageField(upload_to='icones')
+
+    def __str__(self):
+        return self.apelido
